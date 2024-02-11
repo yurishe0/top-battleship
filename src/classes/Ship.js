c62e85ba56cec510484c0f9eb81fdfbb4e911487
yurishe0 <yurishe00@gmail.com>
2024-02-11 16:39:28 +0100

    Implement basic Ship methods

---
+++ src/classes/Ship.js
@@ -0,0 +1,34 @@
+export class Ship {
+    constructor(length) {
+        this.length = length;
+        this.timesHit = 0;
+        this.sunk = false;
+        this.name = this.#asignName(length);
+    }
+
+    #asignName(length) {
+        switch(length) {
+            case 2:
+                return "Destroyer";
+            case 3:
+                return "Battleship";
+            case 4:
+                return "Cruiser";
+            case 5:
+                return "Aircraft Carrier";
+            default:
+                return "Unknown ship";
+        }
+    }
+
+    hit() {
+        this.timesHit += 1;
+    }
+
+    isSunk() {
+        return (this.timesHit === this.length) ? true : false;
+    }
+
+}
+
+module.exports = Ship;
