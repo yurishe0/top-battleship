97b678587451f065c3d5a2f595e932f74091f56a
yurishe0 <yurishe00@gmail.com>
2024-02-26 22:09:02 +0100

    Initialize game

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import Player from "./classes/Player";
+import BotPlayer from "./classes/BotPlayer";
+import DOM from "./ui/DOM";
+
+const root = document.querySelector("#root");
+
+const game = () => {
+    const player1 = new Player("Test");
+    player1.gameboard.placeShip(5, 3, 3, "vertical");
+    player1.gameboard.placeShip(4, 1, 1, "horizontal");
+    player1.gameboard.placeShip(3, 5, 4, "horizontal");
+    player1.gameboard.placeShip(3, 5, 6, "horizontal");
+    player1.gameboard.placeShip(2, 8, 8, "vertical");
+
+    const player2 = new BotPlayer();
+    for(let i = 0; i < 5; i++) {
+        player2.placeRandom();
+    }
+
+    root.appendChild(DOM.createGameboard(player1));
+    root.appendChild(DOM.createGameboard(player2));
+
+
+    // while(!isGameOver(player1, player2)) {
+
+    // }
+}
+
+const isGameOver = (player1, player2) => {
+    return (player1.gameboard.existingShips.length === 0 || player2.gameboard.existingShips.length === 0);
+}
+
+game();
+
+
